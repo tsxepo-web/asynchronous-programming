7f40650bb37edea653e59b252b8641702a9cc348
tsxepo <tsxepo@gmail.com>
2022-11-27 14:28:56 +0200

    Add project files.

---
+++ TheTask/Program.cs
@@ -0,0 +1,66 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Synchronous Operation");
+        RsaPresidents();
+        Console.WriteLine();
+        Console.WriteLine("Asynchronous Operation");
+        RsaPresidentTimePeriod();
+        Console.Read();
+    }
+
+    static void RsaPresidents()
+    {
+        President1();
+        President2();
+        President3();
+        President4();
+    }
+
+    static void RsaPresidentTimePeriod()
+    {
+        Task.Run(() =>
+        {
+            President1();
+        });
+        Task.Run(() =>
+        {
+            President2();
+        });
+        Task.Run(() =>
+        {
+            President3();
+        });
+        Task.Run(() =>
+        {
+            President4();
+        });
+    }
+    static string President1()
+    {
+        Thread.Sleep(300);
+        Console.WriteLine("Nelson Rholihlahla Mandela");
+        return "1994";
+    }
+
+    static string President2()
+    {
+        Thread.Sleep(200);
+        Console.WriteLine("Thabo Mbeki");
+        return "1999";
+    }
+
+    static string President3()
+    {
+        Thread.Sleep(100);
+        Console.WriteLine("Jacob Gedleyihlekisa Zuma");
+        return "2009";
+    }
+
+    static string President4()
+    {
+        Console.WriteLine("Cyril Ramaphosa");
+        return "Current";
+    }
+}
